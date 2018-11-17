7f235234396fa0fe6b54a042dd702442ac925514
imackinn <imackinn@alumni.uwaterloo.ca>
2018-11-17 10:39:00 -0800

    cleaned up code

--- miner.py
+++ miner.py
@@ -1,38 +1,30 @@
-import sys
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdftypes import resolve1
-from pdfminer.pdfpage import PDFPage
-from PIL import Image, ImageDraw
-from pdf2image import convert_from_path
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+from pyqtree import Index
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfdevice import PDFDevice
+from PIL import ImageDraw
+from pdf2image import convert_from_path
 from pdfminer.layout import LAParams
 from pdfminer.converter import PDFPageAggregator
-import pdfminer
-import json
-import glob
 
-from pyqtree import Index
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdftypes import resolve1
+import csv, os, glob, json, pdfminer
 
-from pdfminer.layout import LAParams, LTTextBox,LTChar, LTFigure
-
-def parse_obj(lt_objs, quadtree_index):
+def inject_text_to_quadtree(lt_objs, quadtree):
     # loop over the object list
     for obj in lt_objs:
-        # if it's a textbox, print text and location
+        # if it's a textbox, inject text into quadtree with bounding box index
         if isinstance(obj, pdfminer.layout.LTTextBoxHorizontal):
             field_texts = obj.get_text().split('\n')
             if len(field_texts) == 1:
                 literal_text = obj.get_text().replace('_', '').strip()
-                quadtree_index.insert((literal_text, obj.bbox), obj.bbox)
-                print("%6d, %6d, %s" % (obj.bbox[0], obj.bbox[1], literal_text))
+                quadtree.insert((literal_text, obj.bbox), obj.bbox)
+                #print("%6d, %6d, %s" % (obj.bbox[0], obj.bbox[1], literal_text))
             else:
+                # if there's breaklines in the text, separate them into individual lines for better matching
+                # as a lot of the PDF make heavy use of multi-line text which screws up the label finder
                 i = 0
                 spacing = (obj.bbox[3] - obj.bbox[1]) / len(field_texts)
                 for field_text in field_texts:
@@ -40,168 +32,132 @@ def parse_obj(lt_objs, quadtree_index):
                     new_bbox = [obj.bbox[0], obj.bbox[1] + i*spacing, obj.bbox[2], obj.bbox[1] + spacing]
                     i += 1
                     if len(literal_text.strip()) > 0:
-                        quadtree_index.insert((literal_text, new_bbox), new_bbox)
-                        print("%6d, %6d, %s" % (new_bbox[0], new_bbox[1], literal_text))
+                        quadtree.insert((literal_text, new_bbox), new_bbox)
+                        #print("%6d, %6d, %s" % (new_bbox[0], new_bbox[1], literal_text))
 
-files = glob.glob('original_pdfs/*.pdf')
 
-for filename in files:
-    code = filename.split('/')[1].split('.')[0]
-    json_name = 'output_json/' + code + '.json'
-    print(json_name)
+# returns a hash of all the pages keyed by page_id having a tuple contents of the PNG representing the PDF page and the
+# pages quadtree
+def prepare_pdf_pages(doc, page_images):
+    pdf_processed_pages = {}
 
-    fp = open(filename, 'rb')
-
-    images = convert_from_path(filename)
-
-    parser = PDFParser(fp)
-    doc = PDFDocument(parser, password='')
-
-    #if not doc.is_extractable:
-    #    raise PDFTextExtractionNotAllowed
-
-    # Create a PDF resource manager object that stores shared resources.
-    rsrcmgr = PDFResourceManager()
-
-    # Create a PDF device object.
-    device = PDFDevice(rsrcmgr)
-
-    # BEGIN LAYOUT ANALYSIS
-    # Set parameters for analysis.
+    # prep the PDF interpreter to grab the text fields
     laparams = LAParams()
-
-    # Create a PDF page aggregator object.
+    rsrcmgr = PDFResourceManager()
     device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-
-    # Create a PDF interpreter object.
     interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-    pdfImages = {}
     for i, pdfPage in enumerate(PDFPage.create_pages(doc)):
         quadtree_index = Index(bbox=(0, 0, pdfPage.mediabox[2], pdfPage.mediabox[3]))
-        pdfImages[pdfPage.pageid] = (images[i], images[i].copy(), quadtree_index)
+        pdf_processed_pages[pdfPage.pageid] = (page_images[i], quadtree_index)
 
         # read the page into a layout object
         interpreter.process_page(pdfPage)
         layout = device.get_result()
 
         # extract text from this object
-        parse_obj(layout._objs, quadtree_index)
+        inject_text_to_quadtree(layout._objs, quadtree_index)
+    return pdf_processed_pages
 
-    sanitized_fields = []
 
-    if 'AcroForm' not in doc.catalog:
-        continue
+# returns a tuple of a lot of things; field_id, field_type, field_description, cropped_file_name, match_str
+def process_form_field(field, output_file_code, pdf_processed_pages, make_crops):
+    resolved_field = resolve1(field)
 
-    fields = resolve1(doc.catalog['AcroForm'])['Fields']
-    if type(fields) is not list:
-        continue
+    # gets the details of the form field from the PDF File
+    name, value, rect, page_id = resolved_field.get('T'), resolved_field.get('V'), resolved_field.get(
+        'Rect'), resolved_field.get('P')
 
-    for field in fields:
-        resolved_field = resolve1(field)
-        name, value, rect, page_id = resolved_field.get('T'), resolved_field.get('V'), resolved_field.get('Rect'), resolved_field.get('P')
-        # PDF Rect comes in format
+    field_id = str(field.objid)
 
+    if 'FT' not in resolved_field:
         is_textfield = True
-        if 'FT' not in resolved_field:
-            is_textfield = True
-        elif resolved_field['FT'].name == 'Tx':
-            is_textfield = True
-        elif resolved_field['FT'].name == 'Btn':
-            is_textfield = False
-        else:
-            print('weird form')
+    elif resolved_field['FT'].name == 'Tx':
+        is_textfield = True
+    elif resolved_field['FT'].name == 'Btn':
+        is_textfield = False
+    else:
+        is_textfield = True  # weird form type; assume its a text field
 
-        if page_id is None:
-            continue
+    if page_id is None:
+        return
 
-        page_resolved = resolve1(page_id)
-        draw_image = pdfImages[page_id.objid][1].copy()
-        composite_image = pdfImages[page_id.objid][0]
-        quadtree_index =  pdfImages[page_id.objid][2]
+    page_resolved = resolve1(page_id)
 
-        image_height = draw_image.height
-        image_width = draw_image.width
+    quadtree_index = pdf_processed_pages[page_id.objid][1]
 
-        page_width = page_resolved['MediaBox'][2]
-        page_height = page_resolved['MediaBox'][3]
+    if make_crops:
+        cropped_file_name = 'mturk_images/' + output_file_code + "_" + str(field_id) + ".png"
+        if not os.path.isfile(cropped_file_name):
+            draw_image = pdf_processed_pages[page_id.objid][
+                0].copy()  # makes a copy since we want to make a fresh crop for each one
 
-        scale = image_height / page_height
+            image_height = draw_image.height
 
-        # print('{0}: {1}, {2}'.format(name, value, rect))
+            page_width = page_resolved['MediaBox'][2]
+            page_height = page_resolved['MediaBox'][3]
 
-        x0 = rect[0] * scale
-        y0 = image_height - rect[1] * scale
-        x1 = rect[2] * scale
-        y1 = image_height - rect[3] * scale
-        # [(x0, y0), (x1, y1)] or [x0, y0, x1, y1]
+            scale = image_height / page_height
 
-        draw1 = ImageDraw.Draw(draw_image, 'RGBA')
-        draw1.rectangle([x0, y0, x1, y1], fill=(0,0,250, 100))
+            x0 = rect[0] * scale
+            y0 = image_height - rect[1] * scale
+            x1 = rect[2] * scale
+            y1 = image_height - rect[3] * scale
 
-        draw2 = ImageDraw.Draw(composite_image, 'RGBA')
-        draw2.rectangle([x0, y0, x1, y1], fill=(0,0,250, 100))
+            draw = ImageDraw.Draw(draw_image, 'RGBA')
+            draw.rectangle([x0, y0, x1, y1], fill=(0, 0, 250, 100))
 
-        spacer = 0
-        while True:
-            if spacer > 500:
-                break
-            # keep increasing area until we grab at least one textfield
-            if is_textfield:
-                quadrect = [rect[0] - spacer, rect[1] - spacer, rect[2], rect[3]]
-            else:
-                quadrect = [rect[0], rect[1], rect[2]+ spacer, rect[3] + spacer]
+            crop_area = (0, y1 - 200, page_width * scale, y0 + 200)
+            cropped_example = draw_image.crop(crop_area)
 
-            matches = quadtree_index.intersect(quadrect)
+            cropped_example.save(cropped_file_name, "PNG")
 
-            if len(matches) > 1:
-                break
-            else:
-                spacer += 5
-
-        # handle unfound case
-        if len(matches) == 0:
+    spacer = 0
+    while True:
+        # keep increasing area until we grab at least one textfield or get unreasonably big
+        if spacer > 20:
             break
-
-        # if len(matches) == 1:
-        #     print('test')
-        #else:
-        #     print(len(matches))
-
-        field_id = field.objid
-        match = matches[0]
-        field_description = match[0]
-        quadtree_index.remove(match, match[1])
-
         if is_textfield:
-            field_type = 'string'
+            # textfields have their labels to the left or up
+            quadrect = [rect[0] - spacer, rect[1] - spacer, rect[2], rect[3]]
         else:
-            field_type = 'boolean'
+            # checkboxes have their labels to the right
+            quadrect = [rect[0], rect[1], rect[2] + spacer, rect[3] + spacer]
 
-        sanitized_fields.append((field_id, field_type, field_description))
+        matches = quadtree_index.intersect(quadrect)
 
-        # cropped = img.crop( ( x, y, x + width , y + height ) )
-        # crop_area = (x0-300, y1-200, x1+300, y0+200)
+        if len(matches) > 1:
+            break
+        else:
+            spacer += 5
 
-        # cropped_example = draw_image.crop(crop_area)
+    # handle unfound case
+    if len(matches) == 0:
+        return
 
-        # output_file_name = "d_" + str(field.objid) + ".png"
-        # cropped_example.save('mturk_images/' + output_file_name, "PNG")
+    match = matches[0]
+    field_description = match[0]
+    quadtree_index.remove(match, match[1])
 
-        # break
+    if is_textfield:
+        field_type = 'string'
+    else:
+        field_type = 'boolean'
 
-    data = {}
-    data['fields'] = 'value'
-    json_data = json.dumps(data)
+    match_text = list(map(lambda x: x[0], matches))
+    match_str = ' '.join(match_text)
 
-    #for pdfImage in pdfImages:
-    #    pdfImages[pdfImage][0].show()
+    return field_id, field_type, field_description, cropped_file_name, match_str
 
+
+def create_json_file(filename_code, sanitized_fields):
+    json_name = 'output_json/' + filename_code + '.json'
 
     field_properties = {}
     for field in sanitized_fields:
         field_properties[field[0]] = {'type': field[1], 'description': field[2]}
 
+    data = {}
     fields = {}
     fields['properties'] = field_properties
     fields['type'] = 'object'
@@ -211,4 +167,50 @@ for filename in files:
     with open(json_name, 'w') as f:
         f.write(json_data)
 
-    print(json_data)
+
+def process_pdf_file(filename, csvwriter, make_crops):
+    fp = open(filename, 'rb')
+    output_file_code = filename.split('/')[1].split('.')[0]
+
+    pdf2png_images = convert_from_path(filename)
+
+    parser = PDFParser(fp)
+    doc = PDFDocument(parser, password='')  # pretty much all YG docs are encrypted with an empty password
+
+    pdf_processed_pages = prepare_pdf_pages(doc, pdf2png_images)
+
+    sanitized_fields = []
+
+    if 'AcroForm' not in doc.catalog:
+        return
+
+    fields = resolve1(doc.catalog['AcroForm'])['Fields']
+    if type(fields) is not list:
+        return
+
+    for field in fields:
+        processed_field = process_form_field(field, output_file_code, pdf_processed_pages, make_crops)
+
+        if processed_field is not None:
+            # need to handle None return type on a fail
+            (field_id, field_type, field_description, cropped_file_name, match_str) = processed_field
+
+        if make_crops:
+            csvwriter.writerow((cropped_file_name, match_str))
+        sanitized_fields.append((field_id, field_type, field_description))
+
+    create_json_file(output_file_code, sanitized_fields)
+
+
+if __name__ == "__main__":
+    files = glob.glob('raw_pdfs/*.pdf')
+
+    # makes a csv file contianing references to all the images to be used for mechanical turk with surrounding text blobs
+    with open('mturk.csv', mode='w') as mturk_file:
+        mturk_writer = csv.writer(mturk_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        i = 0
+        for filename in files:
+            print('Processing (',i, ')', filename)
+            i += 1
+            process_pdf_file(filename, mturk_writer, True)
